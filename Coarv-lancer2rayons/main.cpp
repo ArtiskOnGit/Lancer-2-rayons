8aad1b49658ebaad519b86319fa1d94cc09cd7c3
ArtiskOnGit <57273190+ArtiskOnGit@users.noreply.github.com>
2025-02-28 08:38:06 +0100

    initial commit

---
+++ Coarv-lancer2rayons/main.cpp
@@ -0,0 +1,80 @@
+﻿#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#include <iostream>
+
+
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    point3 c_q = center - r.origin();
+    double a = dot(r.direction(), r.direction());
+    double b = dot(-2.0 * r.direction(), c_q);
+    double c = dot(c_q, c_q) - radius * radius;
+
+    double delta = b * b - 4 * a * c;
+    return delta >= 0;
+}
+
+
+color ray_color(const ray& r) {
+
+    vec3 dir = unit_vector(r.direction());
+    auto a = 0.5 * (dir.y() + 1.0);
+
+    if (hit_sphere(point3(0, 0, -1.0), 0.2, r)) {
+        return color(1.0, 0.0 ,0.0);
+    }
+
+    return (1.0 - a) * color(0, 0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+int main() {
+
+    // Image
+
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center
+        - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+}
