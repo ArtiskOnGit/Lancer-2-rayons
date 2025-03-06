339e9050e655f66a9ac6c223560a8042612c8dd8
Biloba <57273190+ArtiskOnGit@users.noreply.github.com>
2025-03-06 15:43:47 +0100

    Before 13 !! (almost finished !!!!!!)

--- Coarv-lancer2rayons/camera.h
+++ Coarv-lancer2rayons/camera.h
@@ -4,13 +4,31 @@
 #include "hittable.h"
 #include "material.h"
 class camera {
+
 public:
     /* Public Camera Parameters Here */
     double aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
     int    samples_per_pixel = 10;   // Count of random samples for each pixel.
     int    max_depth = 10;
+    double vfov = 90;
+    point3 lookfrom = point3(0, 0, 0);   // Point camera is looking from
+    point3 lookat = point3(0, 0, -1);  // Point camera is looking at
+    vec3   vup = vec3(0, 1, 0);     // Camera-relative "up" direction
 
+private:
+    /* Private Camera Variables Here */
+
+    int image_height;
+    double pixel_samples_scale;
+    double focal_length;
+    double viewport_height, viewport_width;
+    vec3 camera_center;
+    vec3 viewport_u, viewport_v, pixel_delta_u, pixel_delta_v;
+    vec3 viewport_upper_left, pixel00_loc;
+    vec3   u, v, w;
+
+public :
     void render(const hittable& world) {
         // Render
         initialize();
@@ -32,17 +50,8 @@ public:
 
     }
 
+
 private:
-    /* Private Camera Variables Here */
-
-    int image_height;
-    double pixel_samples_scale;
-    double focal_length;
-    double viewport_height, viewport_width;
-    vec3 camera_center;
-    vec3 viewport_u, viewport_v, pixel_delta_u, pixel_delta_v;
-    vec3 viewport_upper_left, pixel00_loc;
-
     void initialize() {
 
         // Calculate the image height, and ensure that it's at least 1.
@@ -52,14 +61,25 @@ private:
 
         // Camera
 
-        focal_length = 1.0;
-        viewport_height = 2.0;
+        focal_length = focal_length = (lookfrom - lookat).length();
+        auto theta = degrees_to_radians(vfov);
+        viewport_height = 2 * std::tan(theta / 2) * focal_length;
         viewport_width = viewport_height * (double(image_width) / image_height);
-        camera_center = point3 (0, 0, 0);
+        camera_center = lookfrom;
+
+        //// Calculate the vectors across the horizontal and down the vertical viewport edges.
+        //viewport_u = vec3(viewport_width, 0, 0);
+        //viewport_v = vec3(0, -viewport_height, 0);
+
+        // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
 
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        viewport_u = vec3(viewport_width, 0, 0);
-        viewport_v = vec3(0, -viewport_height, 0);
+        vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
+        vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
+
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         pixel_delta_u = viewport_u / image_width;
@@ -67,7 +87,7 @@ private:
 
         // Calculate the location of the upper left pixel.
         viewport_upper_left = camera_center
-            - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+            -w * focal_length - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
         
